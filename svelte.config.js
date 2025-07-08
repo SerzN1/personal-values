c87d533e107eb941771c50ef4c66bec70ea2823e
Sergei Babin <sergei.babin@ingka.ikea.com>
2025-07-09 01:28:19 +0200

    21

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
 		adapter: adapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
+			pages: 'dist',
+			assets: 'dist',
+			fallback: '404.html',
 			precompress: false,
 			strict: true
-		})
+		}),
+    paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
